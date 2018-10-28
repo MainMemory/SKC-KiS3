955f265e8ec5481ca044a13411a18bd2c0e02c66
MainMemory <mainmemory@yahoo.com>
2018-10-28 12:55:46 -0500

    Version 1.

---
+++ SKC-KiS3/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SKC-KiS3.rc
+//
+#define IDR_OBJECTS_AIZ1                101
+#define IDR_OBJECTS_AIZ2                102
+#define IDR_OBJECTS_CNZ1                103
+#define IDR_OBJECTS_CNZ2                104
+#define IDR_OBJECTS_HCZ1                105
+#define IDR_OBJECTS_HCZ2                106
+#define IDR_OBJECTS_ICZ1                107
+#define IDR_OBJECTS_ICZ2                108
+#define IDR_OBJECTS_LBZ1                109
+#define IDR_OBJECTS_LBZ2                110
+#define IDR_OBJECTS_MGZ1                111
+#define IDR_OBJECTS_MGZ2                112
+#define IDR_RINGS_AIZ1                  113
+#define IDR_RINGS_AIZ2                  114
+#define IDR_RINGS_CNZ1                  115
+#define IDR_RINGS_CNZ2                  116
+#define IDR_RINGS_HCZ1                  117
+#define IDR_RINGS_HCZ2                  118
+#define IDR_RINGS_ICZ1                  119
+#define IDR_RINGS_ICZ2                  120
+#define IDR_RINGS_LBZ1                  121
+#define IDR_RINGS_LBZ2                  122
+#define IDR_RINGS_MGZ1                  123
+#define IDR_RINGS_MGZ2                  124
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        125
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
